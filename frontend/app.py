3dc34989c420a4e80df944cf2fc21e4f1090fcdc
Narcissus <zhuohaoyu1228@gmail.com>
2020-07-19 14:31:49 +0800

    Improved UI

--- frontend/app.py
+++ frontend/app.py
@@ -20,14 +20,33 @@ def query():
     if request.method == 'POST':
         keyword = request.form['queryKeyword']
         output = SrcEng.query(keyword, 20)
-        return render_template('result.html', keyword = keyword, results = output)
+        if len(output) > 0:
+            return render_template('result.html', keyword = keyword, results = output)
+        else:
+            return render_template('query_failed.html', keyword = keyword)
     return render_template('query.html')
 
 @app.route('/query', methods = ['GET'])
 def rawquery():
     word = request.args.get('word')
+    if word != None:
+        results = SrcEng.query_raw(word, 100)
+        return render_template('raw_query.html', results = results)
+    else:
+        return "Bad Arguments"
+
+@app.route('/about', methods = ['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/api', methods = ['GET'])
+def apiquery():
+    word = request.args.get('query')
     results = SrcEng.query_raw(word, 100)
-    return render_template('raw_query.html', results = results)
+    res = ""
+    for result in results:
+        res += result[1].strip() + '\n'
+    return res
 
 if __name__ == "__main__" :
     app.run(host = '0.0.0.0', port = 2333)
